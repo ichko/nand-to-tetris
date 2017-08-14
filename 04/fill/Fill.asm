d61fb51535f4d7efda82575ac9e7db338f9f8ef9
Iliya Zhechev <i4ko95@gmail.com>
2017-08-15 01:36:39 +0300

    Implements screen fill

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,64 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+@SCREEN
+D = A
+
+@address
+M = D // Base screen address
+@8192
+D = A
+@addressCnt
+M = D
+
+(PROBE)
+    @KBD
+    D = M // Probe
+
+    @BLACK
+    D; JNE
+    @WHITE
+    0; JMP
+
+(BLACK)
+    D = -1
+    @SET_SCREEN
+    0; JMP
+
+(WHITE)
+    D = 0
+
+(SET_SCREEN)
+    @multiPixelVal
+    M = D
+    @i
+    M = 1
+
+    @address
+    D = M
+    @currentScreenPos
+    M = D
+
+    (LOOP)
+        @addressCnt
+        D = M
+        @i
+        D = D - M
+        @PROBE
+        D; JLT
+
+        @i
+        M = M + 1
+
+        @multiPixelVal
+        D = M
+        @currentScreenPos
+        A = M
+        M = D
+
+        @currentScreenPos
+        M = M + 1
+
+        @LOOP
+        0; JMP
